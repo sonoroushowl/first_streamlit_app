31378b4d3fb58f58f7ff92fd2fe6fe21fb1d9e19
sonoroushowl <158983842+sonoroushowl@users.noreply.github.com>
2024-02-05 19:12:58 +0800

    Update streamlit_app.py
    Adding header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('A Silly Little Project')
+
+streamlit.header('Oh, what a silly little project')
+streamlit.text('Lorem ipsum dolor sit amet, consectetuer adipiscing elit.')
+streamlit.text('Maecenas porttitor congue massa.')
+streamlit.text('Fusce posuere, magna sed pulvinar ultricies.')
